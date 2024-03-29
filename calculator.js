1772e18cefd5e9a50545b12f62b0e7305f03d554
unknown <hoon81@gmail.com>
2024-03-29 11:44:58 -0400

    Add error checking for operator functions

--- calculator.js
+++ calculator.js
@@ -2,19 +2,23 @@ let num1, num2 = 0;
 let operator = '';
 
 function add(a, b) {
-    return (a + b);
+    if (isNaN(a) || isNaN(b)) return 'ERROR';
+    else return (a + b);
 }
 
 function subtract(a, b) {
-    return (a - b);
+    if (isNaN(a) || isNaN(b)) return 'ERROR';
+    else return (a - b);
 }
 
 function multiply(a, b) {
-    return (a * b);
+    if (isNaN(a) || isNaN(b)) return 'ERROR';
+    else return (a * b);
 }
 
 function divide(a, b) {
-    return (a / b);
+    if (isNaN(a) || isNaN(b) || b===0) return 'ERROR';
+    else return (a / b);
 }
 
 function operate(a, b, op) {
