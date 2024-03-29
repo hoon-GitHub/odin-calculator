7517fc7317345713fcc4761cc611a4c8be4f0f09
unknown <hoon81@gmail.com>
2024-03-29 10:32:33 -0400

    Add operate function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,6 @@
+let num1, num2 = 0;
+let operator = '';
+
 function add(a, b) {
     return (a + b);
 }
@@ -14,3 +17,17 @@ function divide(a, b) {
     return (a / b);
 }
 
+function operate(a, b, op) {
+    switch (op) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+        default:
+            return 'ERROR';
+    }
+}
