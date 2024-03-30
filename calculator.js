bb0dbc78e0967ba117792b0941e03a8e83d6388c
unknown <hoon81@gmail.com>
2024-03-30 18:08:33 -0400

    Add majority of calculator functionality
    Event listeners for buttons for numbers, operators, clear and calculate.
    Took a while to figure out the logic behind regular calculations (using)
    = button vs. consecutive calculations (ex. 12 + 7 - 5 * 3).

--- calculator.js
+++ calculator.js
@@ -1,5 +1,103 @@
-let num1, num2 = 0;
-let operator = '';
+let num1 = null;
+let num2 = null;
+let operator = null;
+let numStor = null;
+let result = null;
+const numBtn = document.querySelectorAll('.num');
+const opBtn = document.querySelectorAll('.op');
+const calcBtn = document.querySelector('.calc');
+const clsBtn = document.querySelector('.cls');
+const dp = document.querySelector('.display');
+
+
+// Event listener for number buttons
+function numClick() {
+    for (let i=0; i < numBtn.length; i++) {
+        numBtn[i].addEventListener('click', function() {
+            if (operator == null) { // operator has not yet been assigned - num1
+                if (numStor == null) { // very first click
+                    numStor = ''; // ready the number storage
+                    numStor += numBtn[i].innerText;
+                    dp.innerText = numStor;
+                } else {
+                    numStor += numBtn[i].innerText;
+                    dp.innerText = numStor;
+                }
+            } else { // operator is there - num2
+                if (numStor == null || num1 == result) { // very first click
+                    numStor = ''; // ready the number storage
+                    numStor += numBtn[i].innerText;
+                    dp.innerText = numStor;
+                } else {
+                    numStor += numBtn[i].innerText;
+                    dp.innerText = numStor;
+                }
+            }
+        });
+    };
+}
+
+// Event listener for operator buttons
+function opClick() {
+    for (let i=0; i < opBtn.length; i++) {
+        opBtn[i].addEventListener('click', function() {
+            if (operator == null) { // operator is missing: set num1 and assign operator
+                if (numStor != null) {
+                    num1 = Number(numStor);
+                    operator = opBtn[i].innerText;
+                    numStor = null;
+                    console.log(`num1: ${num1}, operator: ${operator}`);
+                } else {
+                    return 'ERROR';
+                }
+            } else { // operator is already set: set num2 and calculate
+                if (numStor != null) {
+                    num2 = Number(numStor); // set num2
+                    result = operate(num1, num2, operator); //calculate
+                    dp.innerText = result; // display result
+
+                    num1 = Number(result); // get ready for continued calculation
+                    num2 = null;
+                    numStor = null;
+                    operator = opBtn[i].innerText; // set up new operator
+                    console.log(`num1: ${num1}, operator: ${operator} - consecutive calculation`);
+                }
+            }    
+        });
+    };
+}
+
+// Event listener for calculate (=) button
+function calcClick() {
+    calcBtn.addEventListener('click', () => {
+        if (num1 == null || operator == null || numStor ==null) {
+            result = 'ERROR';
+        } else {
+            num2 = Number(numStor); // finalize num2
+            result = operate(num1, num2, operator); // calculate
+            console.log(`num1: ${num1}, num2: ${num2}, result: ${result}`);
+        }
+        dp.innerText = result; // display result
+        numStor = Number(result); // ready for continued calculation
+        num1 = Number(result);
+        num2 = null;
+        operator = null;
+    });
+}
+
+// Event listener for clear button - simply clear everything
+function clsClick() {
+    clsBtn.addEventListener('click', () => {
+        numStor = null;
+        num1 = null;
+        num2 = null;
+        operator = null;
+        result = null;
+        dp.innerText = '0';
+        console.log('--- CLEAR ---');
+    });
+}
+
 
 function add(a, b) {
     if (isNaN(a) || isNaN(b)) return 'ERROR';
@@ -17,7 +115,7 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    if (isNaN(a) || isNaN(b) || b===0) return 'ERROR';
+    if (isNaN(a) || isNaN(b) || b == 0) return 'ERROR';
     else return (a / b);
 }
 
@@ -35,3 +133,8 @@ function operate(a, b, op) {
             return 'ERROR';
     }
 }
+
+numClick();
+opClick();
+calcClick();
+clsClick();
